64caf4c7bdc7caf6721c1d942c1f0fbf3b8256df
Mario César Señoranis Ayala <mariocesar@creat1va.com>
2014-01-06 12:07:59 -0400

    Setup package

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ django-export2
 ==============
 
 django-export can export results from the admin choosing with fields and relations to include. You can save your queries and share it with other users
+
+Installation
+------------
+
+```
+pip install django-export2
+```
---
+++ setup.py
@@ -0,0 +1,13 @@
+# -*- encoding: utf8 -*-
+
+from __future__ import unicode_literals
+from setuptools import setup, find_packages
+
+
+setup(
+    name='django-export2',
+    version='0.1',
+    author='Mario César Señoranis Ayala',
+    author_email='mariocesar@c1.com.bo',
+    packages=find_packages(),
+)
